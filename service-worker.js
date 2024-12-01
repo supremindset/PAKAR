f853304a9d73fce41e19a9242765a5936fc8e123
supremindset <152413705+supremindset@users.noreply.github.com>
2024-12-01 12:17:07 +0800

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css', // Add your stylesheet if any
+        '/icon.png', // Add your icon
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
